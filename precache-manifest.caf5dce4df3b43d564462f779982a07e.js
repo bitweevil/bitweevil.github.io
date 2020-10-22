79f9fcfba08b81fb9eb5b508b18a519ee254198a
Bitweevil <mark@bitweevil.com>
2020-10-22 18:26:43 +0000

    Updates

---
+++ precache-manifest.caf5dce4df3b43d564462f779982a07e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f600fa7ad9a0e1b2362f284605016a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3333abe0f9436a473fa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d4dd1fe4b4c84631aa09",
+    "url": "/static/js/2.132acea1.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.132acea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3333abe0f9436a473fa",
+    "url": "/static/js/main.1e95d54d.chunk.js"
+  },
+  {
+    "revision": "0568ab7346c18676bb47",
+    "url": "/static/js/runtime-main.07a076ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
